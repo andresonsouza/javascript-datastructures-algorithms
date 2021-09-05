644b0551d7c170056e9f492b097cc6dbd1e72238
Andreson Souza <29142874+andresonsouza@users.noreply.github.com>
2021-09-05 12:03:04 -0300

    Variáveis

---
+++ 02-Variavles.js
@@ -0,0 +1,25 @@
+var num = 1; // {1}
+
+num = 3; // {2}
+
+var price = 1.5; // {3}
+
+var myName = 'Packt'; // {4}
+
+var trueValue = true; // {5}
+
+var nullVar = null; // {6} - Sem valor
+
+var und; // {7} - Declarada mas ainda não recebeu nenhum valor
+
+console.log('num' + num);
+
+console.log('price' + price);
+
+console.log('myName' + myName);
+
+console.log('trueVallue' + trueValue);
+
+console.log('nullVar' + nullVar);
+
+console.log('und' + und);
