7e9efbab7636f41e6a36ba793b862e87a6861791
Andreson Souza <andresonjazz@gmail.com>
2021-09-25 17:15:35 -0300

    Operadores

---
+++ 03-Operators.js
@@ -0,0 +1,77 @@
+var num = 0; //{1}
+console.log(num);
+
+num = num + 2;
+console.log(num);
+
+num = num * 3;
+console.log(num);
+
+num = num / 2;
+console.log(num);
+
+num++;
+console.log(num);
+
+num--;
+console.log(num);
+
+num+=1; //{2}
+console.log(num);
+
+num-=1;
+console.log(num);
+
+num*=3;
+console.log(num);
+
+num/=2;
+console.log(num);
+
+num%=3;
+console.log(num);
+
+console.log('num == 1: ' + (num == 1));//{3}
+
+console.log('num === 1: ' + (num === 1));
+
+console.log('num != 1: ' + (num != 1));
+
+console.log('num > 1: ' + (num > 1));
+
+console.log('num < ' + (num < 1));
+
+console.log('num >= 1: ' + (num >= 1));
+
+console.log('num <= ' + (num <= 1));
+
+console.log('true && false: ' + (true && false));//{4}
+
+console.log('false || true: ' + (false || true));
+
+console.log('!true: ' + (!true))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
