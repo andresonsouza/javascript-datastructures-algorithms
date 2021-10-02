778655923d7ba5f1036c6a8f9be963bb9692259d
Andreson Souza <andresonjazz@gmail.com>
2021-10-02 11:51:54 -0300

    Falso e Verdadeiro

--- 03-Operators.js
+++ 03-Operators.js
@@ -16,19 +16,19 @@ console.log(num);
 num--;
 console.log(num);
 
-num+=1; //{2}
+num += 1; //{2}
 console.log(num);
 
-num-=1;
+num -= 1;
 console.log(num);
 
-num*=3;
+num *= 3;
 console.log(num);
 
-num/=2;
+num /= 2;
 console.log(num);
 
-num%=3;
+num %= 3;
 console.log(num);
 
 console.log('num == 1: ' + (num == 1));//{3}
@@ -51,17 +51,17 @@ console.log('false || true: ' + (false || true));
 
 console.log('!true: ' + (!true));
 
-console.log('5 & 1: ',(5 & 1));
+console.log('5 & 1: ', (5 & 1));
 
-console.log('5 | 1: ',(5 | 1));
+console.log('5 | 1: ', (5 | 1));
 
-console.log('~5 : ',(~5));
+console.log('~5 : ', (~5));
 
-console.log('5 ^ 1: ',(5 ^ 1));
+console.log('5 ^ 1: ', (5 ^ 1));
 
-console.log('5 << 1: ',(5 << 1));
+console.log('5 << 1: ', (5 << 1));
 
-console.log('5 >> 1: ',(5 >> 1));
+console.log('5 >> 1: ', (5 >> 1));
 
 console.log('typeof num: ', typeof num);
 
@@ -69,25 +69,23 @@ console.log('typeof Packt: ', typeof 'Packt');
 
 console.log('typeof true: ', typeof true);
 
-console.log('typeof [1,2,3]: ', typeof [1,2,3]);
+console.log('typeof [1,2,3]: ', typeof [1, 2, 3]);
 
-console.log('typeof {name: John}: ', typeof {name: 'John'});
+console.log('typeof {name: John}: ', typeof { name: 'John' });
 
-var myObj = {name: 'John', age: 21};
+var myObj = { name: 'John', age: 21 };
 delete myObj.age;
 console.log(myObj); //exibe Object {name: "John"}
 
 
+// var carros = {
+//     carro1: 'Fusca',
+//     carro2: 'Celta',
+//     carro3: 'Fiat Uno'
+// }
 
-
-
-
-
-
-
-
-
-
-
-
+// console.log('carros', typeof carros);
+// console.log('carros',carros);
+// delete(carros.carro1);
+// console.log('Novo objeto', carros);
 
