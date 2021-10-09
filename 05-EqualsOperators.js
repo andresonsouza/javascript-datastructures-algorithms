a65e0ecf5ae7759272bfb5b08112ea3dae183671
Andreson Souza <andresonjazz@gmail.com>
2021-10-09 10:48:07 -0300

    Operadores de igualdade

---
+++ 05-EqualsOperators.js
@@ -0,0 +1,13 @@
+console.log('packt' ? true:false); // true
+console.log('' ? true:false); //false
+
+console.log('packt' == true); // false
+console.log('packt' == false); // false
+console.log('packt' === true); // false
+console.log('packt' === 'packt'); // true
+
+var person1 = { name: 'John' };
+var person2 = { name: 'John' };
+
+console.log(person1 === person2); // objetos diferentes
+
