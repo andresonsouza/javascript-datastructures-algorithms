a8ce4bbc1a40be26c25ba552ce29fb3f06a62ede
Andreson Souza <andresonjazz@gmail.com>
2021-11-06 12:48:03 -0300

    Condição falsa

--- 06-ConditionalStatements.js
+++ 06-ConditionalStatements.js
@@ -3,3 +3,11 @@ var num = 1;
 if(num === 1) {
     console.log('num is equal to 1');
 }
+
+var num = 0;
+
+if(num === 1) {
+    console.log('num is equal to 1');
+} else {
+    console.log('num is not equal to 1, the value of num is ' + num);
+}
