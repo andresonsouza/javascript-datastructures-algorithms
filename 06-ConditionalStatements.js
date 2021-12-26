f798f80c51c2fc02767c2e8f985cf6de436304bd
Andreson Souza <andresonjazz@gmail.com>
2021-12-26 12:00:56 -0300

    introduçao aos laços

--- 06-ConditionalStatements.js
+++ 06-ConditionalStatements.js
@@ -74,15 +74,32 @@ if (num === 1) {
 
 var month = 5;
 switch (month) {
-  case 1:
-    console.log('January');
-    break;
-  case 2:
-    console.log('February');
-    break;
-  case 3:
-    console.log('March');
-    break;
-  default:
-    console.log('Month is not January, February or March');
+    case 1:
+        console.log('January');
+        break;
+    case 2:
+        console.log('February');
+        break;
+    case 3:
+        console.log('March');
+        break;
+    default:
+        console.log('Month is not January, February or March');
 }
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+var count = 0;
+while (count < 10) {
+    console.log(i);
+
+    count++;
+}
+
+var x = 0;
+do {
+    console.log(x);
+    x++;
+} while (x < 10);
